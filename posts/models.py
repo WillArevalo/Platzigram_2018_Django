cc6c4a7797cb79bb94e8d51e489d6d564e87d15a
Unknown <willi11-95@hotmail.com>
2018-11-30 16:53:02 -0500

    Intro to Models Django

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,26 @@
+"""Posts models."""
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	"""Model for User"""
+
+	email = models.EmailField(unique=True)
+	password = models.CharField(max_length=100)
+
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+
+	bio = models.TextField(blank=True)
+
+	birthdate = models.DateField(blank=True, null=True)
+
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
+
+	def __init__(self, arg):
+		super(User, self).__init__()
+		self.arg = arg
+
+
+		
