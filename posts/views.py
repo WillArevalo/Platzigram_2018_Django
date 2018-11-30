6efc2ae93ca4eb5beb1b4facf17dd9bcaa2a7f85
Unknown <willi11-95@hotmail.com>
2018-11-30 16:17:23 -0500

    Intro to template system

--- posts/views.py
+++ posts/views.py
@@ -1,37 +1,40 @@
 """Posts Views"""
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 #Utilities
 from datetime import datetime
 
-posts=[
-	{
-		'name':'Mont Blac',
-		'user':'Yésica Cortés',
-		'timestamp': datetime.now().strftime('%b %dth %Y - %H:%M hrs'),
-		'picture': 'https://picsum.photos/200/200/?image=1036',
-	},
+posts = [
     {
-        'name': 'Via Láctea',
-        'user': 'C. Vander',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903',
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name': 'Nuevo auditorio',
-        'user': 'Thespianartist',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076',
+        'photo': 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
 def list_posts(request):
 	"""List existing posts."""
-	content = []
-	for post in posts:
-		content.append("""
-			<p><strong>{name}</strong></p>
-			<p><small>{user} - {timestamp}</small></p>
-			<figure><img src="{picture}" alt="img-post"></figure>
-			""".format(**post))	
-	return HttpResponse('<br>'.join(content))
+	#return render(request,'template', {context})
+	return render(request,'feed.html',{'posts':posts})
