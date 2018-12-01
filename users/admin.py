4e3c6cb1d80e28a9761e68dfd25da144fd418d18
Unknown <willi11-95@hotmail.com>
2018-12-01 16:56:15 -0500

    Register Profile Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
+"""User admin classes."""
+
+#Django
 from django.contrib import admin
 
-# Register your models here.
+#models
+from users.models import Profile
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+	"""Profile Admin"""
+	#Lista de los atributos que mostrara en el admin
+	list_display = ('pk','user','phone_number','website','picture')
+	#Lista de links que llevan al detalle
+	list_display_links = ('pk','user')
+	#Lista de editables in situ
+	list_editable = ('phone_number','website','picture')
+	#Campos en los que se puede buscar
+	search_fields = ('user__email','user__username','user__first_name', 'user__last_name','phone_number')
+	#Campos por los que se puede filtrar
+	list_filter = (
+		'user__is_active',
+		'user__is_staff',
+		'created',
+		'modified',
+	)
