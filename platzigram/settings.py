dff38843a1d570860d56ec564859645dc0e89863
Unknown <willi11-95@hotmail.com>
2018-12-03 13:14:56 -0500

    Login & Login_required

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -135,3 +135,6 @@ STATICFILES_FINDERS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Login
+LOGIN_URL = '/users/login/'
--- platzigram/urls.py
+++ platzigram/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 
 # views
@@ -10,6 +11,7 @@ from django.urls import path
 # or
 from platzigram import views as local_views
 from posts import views as posts_views
+from users import views as users_views
 
 # Para poder visualizar las imgs durante desarrollo se agrega el mas y la linea
 # Ademas se deben cambiar la mediaurl y media root en settings
@@ -17,10 +19,11 @@ urlpatterns = [
     # Admin
     path('admin/', admin.site.urls),
     # main
-    path('hello-world/', local_views.hello_world),
-    path('hi/', local_views.hi),
-    path('say_hi/<str:name>/<int:age>/', local_views.say_hi),
-
+    path('hello-world/', local_views.hello_world, name='hello_world'),
+    path('hi/', local_views.hi, name='hi'),
+    path('say_hi/<str:name>/<int:age>/', local_views.say_hi, name='say_hi'),
+    # Login
+    path('users/login/', users_views.login_view, name='login'),
     # posts
-    path('posts/', posts_views.list_posts),
+    path('posts/', login_required(posts_views.list_posts), name='feed'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
