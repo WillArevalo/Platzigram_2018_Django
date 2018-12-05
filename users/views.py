7075f748d5864fbc36b9b533dcdd031c88d40883
Unknown <willi11-95@hotmail.com>
2018-12-04 20:32:17 -0500

    Form validations

--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import User
 from users.models import Profile
 
 # Forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 # Create your views here.
 
@@ -20,40 +20,13 @@ from users.forms import ProfileForm
 def signup_view(request):
     """Sign up view."""
     if request.method == 'POST':
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_confirmation = request.POST['passwd_confirmation']
-        if passwd != passwd_confirmation:
-            return render(
-                request,
-                'users/signup.html',
-                {'error': 'Password confirmation does not match.'}
-            )
-        try:
-            user = User.objects.create_user(
-                username=username,
-                password=passwd
-            )
-        except IntegrityError as e:
-            return render(
-                request,
-                'users/signup.html',
-                {'error': 'This username already exists. \n Try other'}
-            )
-
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-
-        profile = Profile(user=user)
-        profile.save()
-
-        return render(
-            request,
-            'users/login.html',
-            {'message': 'Your registration was successful. Sign in for continue.'}
-        )
-    return render(request, 'users/signup.html', {'message': False})
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(request, 'users/signup.html', {'form': form})
 
 
 def login_view(request):
